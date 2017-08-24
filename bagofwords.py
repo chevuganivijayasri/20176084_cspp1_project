bd3eac9ae3a5bca172e79c5a57e52ed183c88b05
chevuganivijayasri <31199383+chevuganivijayasri@users.noreply.github.com>
2017-08-25 00:44:43 +0530

    Update bagofwords.py

--- bagofwords.py
+++ bagofwords.py
@@ -1,36 +1,63 @@
-import math
-File1,File2 = open("file1.txt","r"), open("file2.txt","r")
-Read1,Read2 = File1.read().lower(),File2.read().lower()
-def sets(p):
-	dictionary = {}
-	for i in p:
-		if i not in dictionary:
-			dictionary[i] = 1
-		else:
-			dictionary[i] += 1
-	return dictionary
-def squareroot(x,y):
-	s1 = 0
-	s2 = 0
-	for j in x:
-		s1 += (x[j]**2)
-	for k in y:
-		s2 += (y[k]**2)
-	sq1 = math.sqrt(s1)
-	sq2 = math.sqrt(s2)
-	return sq1*sq2
-def dot(x,y):
-	sum=0
-	for i in x:
-		for j in y:
-			if i == j:
-				multiplication = x.get(i)*y.get(j)
-				sum = sum + multiplication
-	return sum
-l = (Read1.split(" "))
-m = (Read2.split(" "))
-x,y = sets(l),sets(m)
-f = squareroot(x,y)
-g = dot(x,y)
-h = (g/f)*100
-print(h ,"% matched")
+import os
+import sys
+class BagOfWords():
+        def __init__(self):
+                self
+        def Input(self,lista):                      #### Creating Dictionary for given file
+                a = {}
+                list1 = open(lista,'r')
+                list1 = list1.read().split(' ')
+                s = "\n@!#$%^&*()_+=|?+-`~';?.,:<>;"
+                for j in s:
+                        for i in range(0, len(list1)):
+                                if j in list1[i]:
+                                        list1[i] = list1[i].lower().replace(j," ")
+                for i in range(len(list1)):
+                    if list1[i] in a:
+                        continue
+                    else:
+                        a[list1[i]] = list1.count(list1[i])
+                return a
+
+        def dotproduct(self,s1,s2):              #Dot product of count of strings that are repeated
+            sum = 0
+            for i in s1:
+                if i in s2:
+                    sum = sum + (s1[i]*s2[i])
+            return sum
+        def Eucledian(self,s):            ####   ----> caluculating eucledian of given set of strings
+            e = 0
+            for i in s:
+                e = e + (s[i]**2)
+            norm = (e)**(1/2)
+            return norm
+        def Percentage(self,a,b):            ####  ---> Plagarism percentage calculation   
+                result=(a/b)*100
+                return result
+        def ReadingFile(self):                     ## ---->Input of directory
+                n=input("Enter the directory name: ")
+                file = []
+                for i in os.listdir(n):
+                    if i.endswith(".txt"):
+                        file.append(i)
+                os.chdir(n)
+                list3=[]
+                list4=[]
+                for i in range(0, len(file)):
+                    list3.append(p.Input(file[i]))
+                print(" ",file)
+                for i in range(len(file)):
+                        for j in range(len(file)):
+                                if(i==j):
+                                    list4.append(0)
+                                else:
+                                    file1 = p.dotproduct(list3[i],list3[j])
+                                    file2 = p.Eucledian(list3[i])
+                                    file3 = p.Eucledian(list3[j])
+                                    file4 = file2*file3
+                                    list4.append(p.Percentage(file1,file4))
+                        print(file[i],list4)
+                        list4=[]
+
+p = BagOfWords()
+p.ReadingFile()
